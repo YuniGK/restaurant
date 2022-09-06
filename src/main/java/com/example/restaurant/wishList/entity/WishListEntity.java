b8052cd0e6b110898d56c3d9e2caaf5bf3df36bb
yunkim <1106qwe@hanmail.net>
2022-09-06 17:59:07 +0900

    뼈대

---
+++ src/main/java/com/example/restaurant/wishList/entity/WishListEntity.java
@@ -0,0 +1,23 @@
+package com.example.restaurant.wishList.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WishListEntity {
+    private String title;               //음식명, 장소명
+    private String category;            //카테고리
+    private String address;             //주소
+    private String readAddress;         //도로명
+    private String homePageLink;        //홈페이지 주소
+    private String imgeLink;            //음식, 가게 이미지 주소
+    private boolean isVisit;            //방문여부
+    private int visitCount;             //방문 카운트
+    private LocalDateTime lastVistDate; //마지막 방문일자
+}
---
+++ src/main/java/com/example/restaurant/wishList/repository/WishListRepository.java
@@ -0,0 +1,4 @@
+package com.example.restaurant.wishList.repository;
+
+public class WishListRepository {
+}
